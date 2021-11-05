e28b4716cc16a5c4b4a90066c772660fb69f903b
rmnk-nz <rua.manukau20@gmail.com>
2021-11-06 08:47:33 +1100

    added routes to view homepage and notes - added endpoint to add new note

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+
+const fs = require('fs');
+const {v4:uuidv4} = require('uuid'); 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+    res.json(notes)
+
+});
+
+app.post('/api/notes', (req, res) => {
+    const addNote = req.body;
+    addNote.id = uuidv4();
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    notes.push(addNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+}); 
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
