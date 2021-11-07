3f81e00b35411aa4fac374995795bd88b68e99bf
rmnk-nz <rua.manukau20@gmail.com>
2021-11-08 06:25:10 +1100

    added app.delete to remove selected saved notes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const fs = require('fs');
 const {v4:uuidv4} = require('uuid'); 
+const { application } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -21,9 +22,8 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
-    res.json(notes)
-
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
@@ -35,7 +35,17 @@ app.post('/api/notes', (req, res) => {
     res.json(notes);
 }); 
 
+app.delete('/api/notes/:id', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    for (let i = 0; i < notes.length; i++)
+      if (notes[i].id === req.params.id) {
+        notes.splice(i, 1);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        return res.json(notes);
+      }
+});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+)
